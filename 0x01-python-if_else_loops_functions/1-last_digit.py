05cd902a0befc6f8bda28d4deb535dfb17dd931b
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-06 20:38:25 +0000

    corrected modulus not handling -ve values

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,6 +9,8 @@ if n > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, n))
 elif n == 0:
     print("Last digit of {} is {} and is 0".format(number, n))
-elif n < 6 and n != 0:
-    print("Last digit of {} is {} and is less than 6 and not 0"\
-    .format(number, n))
+elif n < 6 and n != 0 or number < 0:
+    if number < 0:
+        n = -n
+    print("Last digit of {} is {} and is less than 6 and not 0\
+    ".format(number, n))
