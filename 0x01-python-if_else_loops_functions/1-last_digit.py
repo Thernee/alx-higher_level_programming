acd998ca9ae9fc38bd4c2b891ca84778aff5f3b0
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-06 21:53:42 +0000

    "corrected negative output and wrong output"

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000, 10000)
 
-string = repr(number)
-n = int(string[-1])
+try:
+    number = random.randint(-10000, 10000)
+    string = repr(number)
+    n = int(string[-1])
 
-if n > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, n))
-elif n == 0:
-    print("Last digit of {} is {} and is 0".format(number, n))
-elif n < 6 and n != 0 or number < 0:
-    if number < 0:
-        n = -n
-    print("Last digit of {} is {} and is less than 6 and not 0\
-    ".format(number, n))
+    if n < 6 and n != 0 or number < 0:
+        if number < 0:
+            n = -n
+        print("Last digit of {} is {} and is less than 6 and not 0\
+        ".format(number, n))
+    elif n > 5:
+        print("Last digit of {} is {} and is greater than 5".format(number, n))
+    elif n == 0:
+        print("Last digit of {} is {} and is 0".format(number, n))
+
+except ValueError:
+    print("TypeError")
