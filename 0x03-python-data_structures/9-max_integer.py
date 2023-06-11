076127e7b216f1c15f05c582c19526a0cbf66808
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-11 19:31:29 +0000

    Changed maxx initial value from 0 to my_list[][D0

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,10 +2,10 @@
 
 def max_integer(my_list=[]):
 
-    if not my_list:
+    if my_list == []:
         return None
 
-    maxx = 0
+    maxx = my_list[0]
     for num in my_list:
         if num > maxx:
             maxx = num
