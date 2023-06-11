87a83c96706ebf104bba63d4930bc21f8aac8083
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-11 15:56:16 +0000

    "Removed incorrect check for execution, corrected indentations"

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    # if __name__ == "__main__":
-        for num in my_list:
-            print("{0}".format(num))
+    for num in my_list:
+        print("{0}".format(num))
--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if __name__ == "__main__":
-        if idx < 0:
-            return None
-        elif idx >= len(my_list):
-            return None
-        else:
-            return my_list
+    if idx < 0:
+        return None
+    elif idx >= len(my_list):
+        return None
+    else:
+        return my_list[idx]
