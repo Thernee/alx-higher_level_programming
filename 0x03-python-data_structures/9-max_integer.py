fb2be2266382152bd65f0155e0d068ffb3247952
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-11 19:08:16 +0000

    Corrected indentaion errors

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -7,7 +7,11 @@ def max_integer(my_list=[]):
 
     maxx = 0
     for num in my_list:
-       if num >= maxx:
+        if num >= maxx:
             maxx = num
 
     return maxx
+
+
+mylist = []
+max_integer(mylist)
