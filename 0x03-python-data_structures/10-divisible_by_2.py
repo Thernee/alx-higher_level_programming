59301080b9ccddadebe4e6f826fc380200ab4320
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-11 19:59:06 +0000

    removed test code in 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,13 +4,3 @@ def divisible_by_2(my_list=[]):
 
     new_list = [True if num % 2 == 0 else False for num in my_list]
     return new_list
-
-
-
-my_list = [0, 1, 2, 3, 4, 5, 6]
-list_result = divisible_by_2(my_list)
-
-i = 0
-while i < len(list_result):
-    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
-    i += 1
