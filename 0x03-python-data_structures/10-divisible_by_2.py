6918c4b796fe2446f6ad7a9aed9a6a01a0838819
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-12 13:58:59 +0000

    corrected header: Added /usr

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,12 +1,6 @@
-#!/bin/python3
+#!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
     if my_list:
-        new_list = []
-        # new_list = [True if num % 2 == 0 else False for num in my_list]
-        for num in my_list:
-            if num % 2 == 0:
-                new_list.append(True)
-            else:
-                new_list.append(False)
+        new_list = [True if num % 2 == 0 else False for num in my_list]
         return new_list
