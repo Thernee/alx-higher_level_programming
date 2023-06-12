415d8f4141f6c814dd7130bf7c9dafb99e5fead6
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-12 13:54:40 +0000

    Changed new_list assignment from list comprehension

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,5 +3,10 @@
 def divisible_by_2(my_list=[]):
     if my_list:
         new_list = []
-        new_list = [True if num % 2 == 0 else False for num in my_list]
+        # new_list = [True if num % 2 == 0 else False for num in my_list]
+        for num in my_list:
+            if num % 2 == 0:
+                new_list.append(True)
+            else:
+                new_list.append(False)
         return new_list
