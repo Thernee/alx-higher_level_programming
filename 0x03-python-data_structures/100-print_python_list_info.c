03399114b0fa1b124ca5a9330c4ef2c9ffe90689
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-12 20:55:31 +0000

    Added 13-is_palindrome.c

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * print_python_list_info - prints the information of a python list
+ *
+ * @p: Python object to be operated on
+ * Return: nothing
+ */
+void print_python_list_info(PyObject *p)
+{
+	int holder;
+}
---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * is_palindrome - checks if a single linked list is a palindrome
+ *
+ * @head: pointer to the head of the list
+ * Return: 1 if list is palindrome, 0 if not
+ */
+int is_palindrome(listint_t **head)
+{
+	ahead = *head;
+}
