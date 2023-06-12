b77482dd2761728288290b738ccb135f52e526b3
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-12 21:57:53 +0000

    Updated code for 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,5 +7,39 @@
  */
 int is_palindrome(listint_t **head)
 {
+	listint_t *ahead;
+	listint_t *behind;
+	int count = 0;
+	int i, j;
+
 	ahead = *head;
+	behind = *head;
+
+	if (*head == NULL)
+		return (1);
+
+	while (ahead->next != NULL)
+	{
+		count++;
+		ahead = ahead->next;
+	}
+	ahead = *head;
+
+	for (i = 1; i <= count; i++)
+	{
+		j =1;
+		while (j <= count - i)
+		{
+			if (ahead->next != NULL)
+				ahead = ahead->next;
+			j++;
+		}
+		if (ahead->n != behind->n)
+			return (0);
+
+		behind = behind->next;
+		ahead = behind;
+	}
+
+	return (1);
 }
