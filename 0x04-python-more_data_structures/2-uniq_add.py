777bae9c838d6d9af72e7b55931649327542f61d
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-14 20:49:18 +0000

    Updated 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    if my_list:
-        holder = set(my_list)
-        result = 0
+    # if my_list:
+    holder = set(my_list)
+    result = 0
 
-        for num in holder:
-            result += int(num)
-        return result
+    for num in holder:
+        result += int(num)
+    return result
 
-    return my_list
+    # return my_list
