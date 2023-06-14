91b5f305475a3c1b10fd7456eb4aa251565b05d9
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-14 21:18:23 +0000

    Corrected element check operator from '|' to '^'

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    if set_1:
-        if set_2:
-            holder = set_1 | set_2
-            return sorted(holder)
-        return sorted(set_1)
-
-    return sorted(set_2)
+    holder = set_1 ^ set_2
+    return sorted(holder)
