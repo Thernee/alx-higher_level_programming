a5227c230387edfa8d5dc7d27b55e3226bb17d23
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-14 21:32:08 +0000

    Updated print() to display key as string

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,5 +2,5 @@
 
 def print_sorted_dictionary(a_dictionary):
 
-    for key, val in sorted(a_dictionary):
-        print("{}: {}".format(key, val))
+    for key in sorted(a_dictionary):
+        print("{}: {}".format(key, a_dictionary.get(key)))
