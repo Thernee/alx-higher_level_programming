2fe9a51ddef7df029054699efe0c02232a9e968f
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-14 23:30:37 +0000

    Added whitespace around operator in 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -5,6 +5,6 @@ def multiply_by_2(a_dictionary):
     multiplied_dict = {}
     for key in a_dictionary.keys():
         holder = a_dictionary[key]
-        multiplied_dict[key] = holder *2
+        multiplied_dict[key] = holder * 2
 
     return multiplied_dict
