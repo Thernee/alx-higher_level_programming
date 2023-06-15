8a79e3dbda778ad7bbaf7faea3eb20eb8990704c
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-15 00:30:32 +0000

    Reduced characters on line 3 of 101-square_matrix_map.py to satisfy pycodestyle

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/pytjon3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda sublist: list(map(lambda num: num ** 2, sublist)), matrix))
+    return list(map(lambda sub_l: list(map(lambda n: n ** 2, sub_l)), matrix))
