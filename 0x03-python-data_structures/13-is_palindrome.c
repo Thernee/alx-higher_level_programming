572eb52b3e4ababf9c3898548a151114c4839151
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-24 19:59:37 +0000

    corrected efficiency issues in 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,45 +1,39 @@
 #include "lists.h"
+
 /**
  * is_palindrome - checks if a single linked list is a palindrome
  *
  * @head: pointer to the head of the list
- * Return: 1 if list is palindrome, 0 if not
+ * Return: 1 if list is a palindrome, 0 if not
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *ahead;
-	listint_t *behind;
-	int count = 1;
-	int i, j;
+	listint_t *ahead, *behind;
+	int count = 0, i, j;
 
-	ahead = *head;
-	behind = *head;
-
-	if (*head == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
-	while (ahead->next != NULL)
+	ahead = behind = *head;
+
+	while (ahead != NULL)
 	{
 		count++;
 		ahead = ahead->next;
 	}
-	ahead = *head;
 
-	for (i = 1; i < count; i++)
+	for (i = 0; i < count / 2; i++)
 	{
-		j =1;
-		while (j <= count - i)
-		{
-			if (ahead->next != NULL)
-				ahead = ahead->next;
-			j++;
-		}
-		if (ahead->n != behind->n)
+		ahead = *head;
+		for (j = 0; j < count - i - 1; j++)
+			ahead = ahead->next;
+
+		if (behind->n != ahead->n)
 			return (0);
 
 		behind = behind->next;
-		ahead = *head;
 	}
 
 	return (1);
 }
+
