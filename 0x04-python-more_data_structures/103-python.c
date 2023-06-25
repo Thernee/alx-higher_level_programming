6493b386bc5e421870ebd1185efc7367961833b7
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-25 17:43:22 +0000

    reverted 103-python.c to previous change

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,5 +1,4 @@
 #include <Python.h>
-
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
 
@@ -9,24 +8,24 @@ void print_python_bytes(PyObject *p);
  */
 void print_python_list(PyObject *p)
 {
-	Py_ssize_t size, i;
+	Py_ssize_t size, alloc, i;
 	const char *type;
+	PyListObject *list = (PyListObject *)p;
+	PyVarObject *var = (PyVarObject *)p;
 
-	size = PyObject_Length(p);
+	size = var->ob_size;
+	alloc = list->allocated;
 
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %zd\n", size);
-	printf("[*] Allocated = %zd\n", size);
+	printf("[*] Allocated = %zd\n", alloc);
 
 	for (i = 0; i < size; i++)
 	{
-		PyObject *item = PySequence_GetItem(p, i);
-
-		type = item->ob_type->tp_name;
+		type = list->ob_item[i]->ob_type->tp_name;
 		printf("Element %zd: %s\n", i, type);
-
 		if (strcmp(type, "bytes") == 0)
-			print_python_bytes(item);
+			print_python_bytes(list->ob_item[i]);
 	}
 }
 
@@ -38,31 +37,27 @@ void print_python_bytes(PyObject *p)
 {
 	Py_ssize_t size, i, limit;
 	unsigned char *str;
+	PyVarObject *var_obj = (PyVarObject *)p;
 
 	printf("[.] bytes object info\n");
 
-	if (!PyBytes_Check(p))
+	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
 
-	size = ((PyVarObject *)p)->ob_size;
-	str = (unsigned char *)PyString_AsString(p);
+	size = var_obj->ob_size;
+	str = (unsigned char *)var_obj->ob_sval;
 
 	printf("  size: %zd\n", size);
 	printf("  trying string: %s\n", str);
 
 	limit = size <= 10 ? size : 10;
-
 	printf("  first %zd bytes:", limit);
 
 	for (i = 0; i < limit; i++)
-		printf(" %02x", str[i]);
-
-	if (size < 10)
-		printf(" 00");
+		printf(" %02hhx", str[i]);
 
 	printf("\n");
 }
-
