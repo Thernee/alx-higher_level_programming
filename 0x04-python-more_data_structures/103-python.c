39c4b5bde5e8be41e4436f7b1b7c3fc2dbeba6e9
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-25 17:49:38 +0000

    trying to revert 103-python.c to previos change again

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,7 +1,6 @@
 #include <Python.h>
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
-
 /**
  * print_python_list - Prints basic info about Python lists.
  * @p: A PyObject list object.
@@ -10,25 +9,23 @@ void print_python_list(PyObject *p)
 {
 	Py_ssize_t size, alloc, i;
 	const char *type;
-	PyListObject *list = (PyListObject *)p;
-	PyVarObject *var = (PyVarObject *)p;
 
-	size = var->ob_size;
-	alloc = list->allocated;
-
+	size = PyList_Size(p);
+	alloc = ((PyListObject *)p)->allocated;
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %zd\n", size);
 	printf("[*] Allocated = %zd\n", alloc);
 
 	for (i = 0; i < size; i++)
 	{
-		type = list->ob_item[i]->ob_type->tp_name;
+		PyObject *item = PySequence_GetItem(p, i);
+
+		type = item->ob_type->tp_name;
 		printf("Element %zd: %s\n", i, type);
 		if (strcmp(type, "bytes") == 0)
-			print_python_bytes(list->ob_item[i]);
+			print_python_bytes(item);
 	}
 }
-
 /**
  * print_python_bytes - Prints basic info about Python byte objects.
  * @p: A PyObject byte object.
@@ -37,27 +34,20 @@ void print_python_bytes(PyObject *p)
 {
 	Py_ssize_t size, i, limit;
 	unsigned char *str;
-	PyVarObject *var_obj = (PyVarObject *)p;
 
 	printf("[.] bytes object info\n");
-
-	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
+	if (!PyObject_IsInstance(p, (PyObject *)&PyBytes_Type))
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
-
-	size = var_obj->ob_size;
-	str = (unsigned char *)var_obj->ob_sval;
-
+	size = PyBytes_Size(p);
+	str = (unsigned char *)PyBytes_AsString(p);
 	printf("  size: %zd\n", size);
 	printf("  trying string: %s\n", str);
-
 	limit = size <= 10 ? size : 10;
 	printf("  first %zd bytes:", limit);
-
 	for (i = 0; i < limit; i++)
-		printf(" %02hhx", str[i]);
-
+		printf(" %02x", str[i]);
 	printf("\n");
 }
