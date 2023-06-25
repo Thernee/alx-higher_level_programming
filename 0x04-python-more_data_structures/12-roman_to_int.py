be370694dbb36b41ea18e36ec4d298475c56c205
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-25 17:59:12 +0000

    Added 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,30 @@
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_values = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    total = 0
+    prev_value = 0
+
+    for roman_char in roman_string:
+        value = roman_values.get(roman_char, 0)
+
+        if value == 0:
+            return 0
+
+        if value > prev_value:
+            total -= 2 * prev_value
+
+        total += value
+        prev_value = value
+
+    return total
