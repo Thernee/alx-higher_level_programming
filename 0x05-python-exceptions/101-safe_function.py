b3d0967610c46edd8644bf3eb74e1934459f04b8
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-26 23:45:24 +0000

    fixed pycodestyle issue in 101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ import sys
 def safe_function(fct, *args):
     try:
         holder = fct(*args)
-    except:
+    except BaseException:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return None
     else:
