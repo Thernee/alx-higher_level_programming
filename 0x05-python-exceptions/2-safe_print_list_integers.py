2d502f03e19c130c1feb38f1036d97347471e5ee
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-26 20:49:09 +0000

    moved try block into loop in 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0 
-    try:
-        for num in range(0, x):
+    count = 0
+
+    for num in range(0, x):
+        try:
             print("{:d}".format(my_list[num]), end="")
             count += 1
-    except ValueError:
-        pass
-    finally:
-        print()
-        return count
+        except (ValueError, TypeError):
+            pass
+    print()
+    return count
