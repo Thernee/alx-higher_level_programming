cdcc3b7ddfabf943fdcb2fd289664e69983c577e
Abubakar Adam Sani <Athernee@gmail.com>
2023-07-03 22:48:26 +0000

    Added setters and getters in 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,4 +5,44 @@
 
 class Rectangle:
     """ The rectangle class."""
-    pass
+    def __init__(self, width=0, height=0):
+        """ Initializes the rectangle
+
+        Args:
+            width (int): Given size of the rectangle
+            height (int): Given size of the rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def height(self):
+        """ Returns the current Rectangle height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ setter for the rectangle height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            else:
+                self.__height = value
+
+    @property
+    def width(self):
+        """ Returns the current Rectangle width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ setter for the rectangle width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+               self.__width = value
