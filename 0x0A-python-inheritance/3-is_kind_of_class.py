27d5d3fe22e269d76d4593f0e37da969e52d1bf2
Abubakar Adam Sani <Athernee@gmail.com>
2023-07-10 21:11:28 +0000

    corrected 2-is_same_class.py check

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,8 +3,8 @@
 """ A class that checks for instance of an object"""
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """Define the class"""
-    if type(obj) is not a_class:
-        return False
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return True
     return False
