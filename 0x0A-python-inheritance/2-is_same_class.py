e7f86f0c62cd78e0d258e4afa94eca0ccbd61bee
Abubakar Adam Sani <Athernee@gmail.com>
2023-07-10 21:04:55 +0000

    Added 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 
+""" A class that checks for instance of an object"""
 
+
+def is_same_class(obj, a_class):
+    """Define the class"""
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return True
+    return False
