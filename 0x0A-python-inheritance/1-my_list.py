26a9664b282dd811f67d232ad916d8029bfa63cd
Abubakar Adam Sani <Athernee@gmail.com>
2023-07-10 17:28:49 +0000

    Added 1-my_list.py and tests/1-my_list.txt

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 
+""" An implememtation of MyList subclass"""
 
+
+class MyList(list):
+    """ An implememtation of MyList subclass"""
+    def __init__(self):
+        """ Initialize the MyList subclass"""
+#self.list = []
+
+    def print_sorted(self):
+        """ Print a sorted list"""
+        print(sorted(self))
