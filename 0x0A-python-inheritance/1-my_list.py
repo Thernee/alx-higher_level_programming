b736cbe6c6944195fc557b55ac54b75173661bb4
Abubakar Adam Sani <Athernee@gmail.com>
2023-07-10 17:30:35 +0000

    removed block comment from 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,7 +7,7 @@ class MyList(list):
     """ An implememtation of MyList subclass"""
     def __init__(self):
         """ Initialize the MyList subclass"""
-#self.list = []
+        self.list = []
 
     def print_sorted(self):
         """ Print a sorted list"""
