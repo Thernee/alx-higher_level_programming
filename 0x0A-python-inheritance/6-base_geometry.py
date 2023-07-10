d114c3028f7e6c39742cbd03cbb96bb23e85b26a
Abubakar Adam Sani <Athernee@gmail.com>
2023-07-10 21:43:49 +0000

    Added 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" A definition of BaseGeometry class"""
+
+
+class BaseGeometry():
+    """ Define BaseGeometry class"""
+    pass
+
+    def area(self):
+        """ return area of a geometry shape"""
+        raise Exception("area() is not implemented")
