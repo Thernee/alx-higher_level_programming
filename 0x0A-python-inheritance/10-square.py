2927702ba4dedabec0c22938fe4fc25d70d175f9
Abubakar Adam Sani <Athernee@gmail.com>
2023-07-11 00:10:25 +0000

    Added 10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" A definition of Square class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Define Square class"""
+    def __init__(self, size):
+        """ Initialize Square class"""
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Return area of the rectangle."""
+        return self.__size * self.__size
