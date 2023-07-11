64fb97a998289330e9950fb85c3d3c314890241d
Abubakar Adam Sani <Athernee@gmail.com>
2023-07-11 23:18:26 +0000

    fixed pycodestyle issue in 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,7 +6,8 @@ from sys import argv
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
     my_list = []
     try:
@@ -19,5 +20,3 @@ if __name__ == "__main__":
             my_list.append(argv[num])
 
     save_to_json_file(my_list, "add_item.json")
-
-
