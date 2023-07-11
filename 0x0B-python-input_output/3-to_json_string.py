3e8e33370c086c931228bb857a9460c39330ee40
Abubakar Adam Sani <Athernee@gmail.com>
2023-07-11 22:03:17 +0000

    Added 3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+""" A function that returns JSON repr. of an object"""
+import json
+
+def to_json_string(my_obj):
+    """ Define to_json_string function"""
+    return json.dumps(my_obj)
