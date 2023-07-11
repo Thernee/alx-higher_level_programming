7fd1de468628198a5fe554a2307820b75e770593
Abubakar Adam Sani <Athernee@gmail.com>
2023-07-11 22:40:39 +0000

    updated load() in 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,5 +7,5 @@ import json
 def load_from_json_file(filename):
     """ Define save_to_json_file function"""
     with open(filename, 'r', encoding='utf-8') as s_file:
-        obj = json.load(my_obj, s_file)
+        obj = json.load(s_file)
     return obj
