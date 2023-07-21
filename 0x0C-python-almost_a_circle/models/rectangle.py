0a9218b8c5e07299eb89d30bc3e49e424a7511e4
Abubakar Adam Sani <Athernee@gmail.com>
2023-07-21 22:02:50 +0000

    handled **kwargs in models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -87,15 +87,21 @@ class Rectangle(Base):
         total = f" - {self.__width}/{self.__height}"
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y}{total}"
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """Update values of Rectangle with positional arguments"""
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.width = args[1]
-        if len(args) >= 3:
-            self.height = args[2]
-        if len(args) >= 4:
-            self.x = args[3]
-        if len(args) >= 5:
-            self.y = args[4]
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+
+        else:
+            if kwargs and len(kwargs) >= 1:
+                for key, value in kwargs.items():
+                    setattr(self, key, value)
