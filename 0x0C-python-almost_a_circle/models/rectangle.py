8bea3c743befd47f3b94b1d922a2fcb32f5eaa38
Abubakar Adam Sani <Athernee@gmail.com>
2023-07-21 21:46:22 +0000

    Added update() to models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -86,3 +86,16 @@ class Rectangle(Base):
         """Return string representation of rectangle instance"""
         total = f" - {self.__width}/{self.__height}"
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y}{total}"
+
+    def update(self, *args):
+        """Update values of Rectangle with positional arguments"""
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
