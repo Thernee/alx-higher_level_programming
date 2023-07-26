1fd84e18914156db865e520e4891c530b962c2b3
Abubakar Adam Sani <Athernee@gmail.com>
2023-07-26 21:12:10 +0000

    Added files for tasks 16-19

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,3 +26,48 @@ class Base():
         if list_dictionaries is None or not list_dictionaries:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON serialization of list of objects to a file."""
+        filename = cls.__name__ + ".json"
+        with open(filename, "w", encoding="utf-8") as json_file:
+            if list_objs is None:
+                json_file.write("[]")
+            else:
+                my_list = [obj.to_dictionary() for obj in list_objs]
+                json_file.write(Base.to_json_string(my_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return python representation of json string"""
+        if not json_string:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """create a new instance from dictionary"""
+        if cls.__name__ == "Rectangle":
+            new_instance = cls(5, 5)
+        elif cls.__name__ == "Square":
+            new_instance = cls(5, 5)
+        new_instance.update(**dictionary)
+        return new_instance
+
+    @classmethod
+    def load_from_file(cls):
+        """load from file"""
+        filename = cls.__name__ + ".json"
+        obj = []
+
+        try:
+            with open(filename, 'r') as f:
+                str = f.read().strip()
+                holder = cls.from_json_string(str)
+                for elem in holder:
+                    obj.append(cls.create(**elem))
+        except Exception:
+            return []
+
+        return obj
