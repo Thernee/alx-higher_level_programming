73ccac87cff9d690f67bd3bae3866c6b7e1ba8f3
Abubakar Adam Sani <Athernee@gmail.com>
2023-07-26 13:18:01 +0000

    Handled *args and **kwargs in models/square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -27,3 +27,22 @@ class Square(Rectangle):
         """Setr size of Square"""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """Update values of Square with positional arguments"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+
+        else:
+            if kwargs and len(kwargs) >= 1:
+                for key, value in kwargs.items():
+                    setattr(self, key, value)
