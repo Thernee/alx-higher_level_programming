f5ad0456d11b5930bafde26cf79af2dcf114461e
Abubakar Adam Sani <Athernee@gmail.com>
2023-07-29 21:27:58 +0000

    fixed 100-matrix_mul.py

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -20,11 +20,11 @@ def matrix_mul(m_a, m_b):
     if not all(isinstance(row, list) for row in m_b):
         raise TypeError("m_b must be a list of lists")
 
-    if all([not isinstance(ele, int) and not isinstance(ele, float)
-            for row in m_a for ele in row]):
+    if not all((isinstance(ele, int) or isinstance(ele, float))
+               for ele in [num for row in m_a for num in row]):
         raise TypeError("m_a should contain only integers or floats")
-    if not any([isinstance(ele, int) or isinstance(ele, float)
-                for row in m_b for ele in row]):
+    if not all((isinstance(ele, int) or isinstance(ele, float))
+               for ele in [num for row in m_b for num in row]):
         raise TypeError("m_b should contain only integers or floats")
 
     if not all(len(row) == len(m_a[0]) for row in m_a):
