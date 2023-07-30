e662379c1bf7d7c53391c5c6f6ebf244bc5c2b38
Abubakar Adam Sani <Athernee@gmail.com>
2023-07-30 14:06:38 +0000

    Added 102-python.c

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <Python.h>
+
+/**
+ * print_python_string - Prints python strings
+ *
+ * @p: Python Object
+ * Return: nothing
+ */
+void print_python_string(PyObject *p)
+{
+
+	PyObject *string;
+
+	printf("[.] string object info\n");
+
+	if (strcmp(p->ob_type->tp_name, "string"))
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+
+	string = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
+	printf("  length: %ld\n", PyUnicode_GET_SIZE(p));
+	printf("  value: %s\n", PyBytes_AsString(string));
+}
