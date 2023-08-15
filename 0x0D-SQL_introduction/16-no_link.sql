0b39d3afcabc4cc39fd282e2b3be27ef57e6dab4
Abubakar Adam Sani <Athernee@gmail.com>
2023-08-15 17:42:47 +0000

    updated 16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 -- lists all records of the table second_table
 -- not listing  rows without a name value
 
-SELECT name, score FROM `second_table`
-WHERE name <> ''
-ORDER BY score DESC;
+SELECT `name`, `score` FROM `second_table`
+WHERE `name` <> ""
+ORDER BY `score` DESC;
