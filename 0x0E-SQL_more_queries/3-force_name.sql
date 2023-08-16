723bcc612d6851a2d64047d9d027bf7ab5c838a9
Abubakar Adam Sani <Athernee@gmail.com>
2023-08-16 21:47:45 +0000

    corrected id name error

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,3 @@
 -- creates creates the table force_name
 
-CREATE TABLE IF NOT EXISTS `force_name` (`Id` INT DEFAULT 1, `name` VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS `force_name` (`id` INT, `name` VARCHAR(256) NOT NULL);
