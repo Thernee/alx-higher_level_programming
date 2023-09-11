068a52e93f5bc7465f7128ab5f60fd8cc751021e
Abubakar Adam Sani <Athernee@gmail.com>
2023-09-11 19:42:44 +0100

    Fix: 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
 let argLen = (parseInt(process.argv[2]));
+if (!argLen) console.log('Missing number of occurrences');
 
-while (argLen) {
+while (argLen > 0) {
   console.log('C is fun');
   argLen--;
 }
