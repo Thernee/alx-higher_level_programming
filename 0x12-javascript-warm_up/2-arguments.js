1507a9636da07e12deaa06be240dcea66bc9b550
Abubakar Adam Sani <Athernee@gmail.com>
2023-09-11 17:12:15 +0100

    fix: 2-arguments    Add: 3-value_arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,5 +4,5 @@
 const argLength = (process.argv.length - 2);
 
 if (argLength === 0) console.log('No argument');
-if (argLength === 1) console.log('Argument found');
-if (argLength > 1) console.log('Arguments found');
+else if (argLength === 1) console.log('Argument found');
+else if (argLength > 1) console.log('Arguments found');
