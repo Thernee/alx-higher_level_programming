0939cb2c4b35fb09603c2b4616ddda12436ca27f
Abubakar Adam Sani <Athernee@gmail.com>
2023-09-17 11:40:45 +0100

    Fixed: port issue in files 0-2

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,8 +5,8 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2],
-                         db=argv[3])
+    db = MySQLdb.connect(host='localhost', user=argv[1], passwd=argv[2],
+                         db=argv[3], port=3306)
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states")
     states = cursor.fetchall()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,8 +5,8 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2],
-                         db=argv[3])
+    db = MySQLdb.connect(host='localhost', user=argv[1], passwd=argv[2],
+                         db=argv[3], port=3306)
     cursor = db.cursor()
     query = """SELECT * FROM states WHERE `name` = '{}'
                    ORDER BY `id`"""
