a628ac903d56208faa160548637585b2633195ea
Abubakar Adam Sani <Athernee@gmail.com>
2023-09-22 21:42:02 +0100

    Fixed: file 1 and 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
     states = cursor.fetchall()
 
     for state in states:
-        print(state)
+        if state[1][0] == 'N':
+            print(state)
 
     cursor.close()
     db.close()
