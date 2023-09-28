fe13382d4e9a171bcfed363cbf6d3190ba066664
Abubakar Adam Sani <Athernee@gmail.com>
2023-09-28 20:35:32 +0000

    Fix: 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept
-curl -sH "X-School-User-Id : 98" "$1"
+curl -sH "X-School-User-Id: 98" "$1"
--- 0x10-python-network_0/README.md
+++ 0x10-python-network_0/README.md
@@ -1,1 +1,3 @@
 ## Project: 0x10. Python - Network #0
+
+# Performing various networking tasks using cURL and bash scripts
