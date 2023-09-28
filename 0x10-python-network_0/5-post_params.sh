63252f5723902cd51f4193b9b6d9bdbc6100a8ba
Abubakar Adam Sani <Athernee@gmail.com>
2023-09-28 20:53:37 +0000

    Fix: 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and sends a POST request with email and subject
-curl -sX POST -H "email: test@gmail.com" -H "subject: I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
