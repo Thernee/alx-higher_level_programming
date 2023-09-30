ff89b60452743ce6d1c2703ea0d227bf68500c2e
Abubakar Adam Sani <Athernee@gmail.com>
2023-09-30 14:49:33 +0000

    Added checkes in 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,8 +4,9 @@
 import urllib.request
 from sys import argv
 
-req = urllib.request.Request(argv[1])
-with urllib.request.urlopen(req) as response:
+if __name__ == "__main__":
+    req = urllib.request.Request(argv[1])
+    with urllib.request.urlopen(req) as response:
 
-    id_val = response.headers.get('X-Request-Id')
-    print(id_val)
+        id_val = response.headers.get('X-Request-Id')
+        print(id_val)
