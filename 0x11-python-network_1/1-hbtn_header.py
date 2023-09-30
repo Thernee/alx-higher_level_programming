6f4b460b90bd71dee9cc9c36149b70d98a85d688
Abubakar Adam Sani <Athernee@gmail.com>
2023-09-30 14:44:28 +0000

    Update: 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,7 +4,8 @@
 import urllib.request
 from sys import argv
 
-with urllib.request.urlopen(argv[1]) as response:
+req = urllib.request.Request(argv[1])
+with urllib.request.urlopen(req) as response:
 
     id_val = response.headers.get('X-Request-Id')
     print(id_val)
