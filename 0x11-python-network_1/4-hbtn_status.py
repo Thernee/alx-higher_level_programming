4c2dcd1d23c6d3af608491f13fb0e921b1b326d2
Abubakar Adam Sani <Athernee@gmail.com>
2023-10-01 14:04:35 +0000

    Fix: 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,7 +3,8 @@
 """Uses requets to fetch https://alx-intranet.hbtn.io/status. and print it"""
 import requests
 
-req = requests.get('https://alx-intranet.hbtn.io/status')
-holder = response.read()
-print(f'Body response:\n\t- type: {type(holder.text)}')
-print(f'\t- content: {holder.text}')
+if __name__ == "__main__":
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    holder = response.read()
+    print(f'Body response:\n\t- type: {type(holder.text)}')
+    print(f'\t- content: {holder.text}')
