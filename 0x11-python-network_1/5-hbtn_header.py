6e23891a1c37a740f209296bf07f5e2080242c8a
Abubakar Adam Sani <Athernee@gmail.com>
2023-10-01 14:12:51 +0000

    Add: 5-hbtn_header.py

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Uses requests to fetch 'X-Request-Id' from a passed url"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    response = requests.get(argv[i])
+
+    id_val = response.headers['X-Request-Id']
+    print(id_val)
