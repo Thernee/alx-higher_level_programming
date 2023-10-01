27da9712cd2798d8c34b047bb1ea2c48285e56ac
Abubakar Adam Sani <Athernee@gmail.com>
2023-10-01 15:28:47 +0000

    Add: 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""sends request to a URL and displays body of the response, or error"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    if argv[2]:
+        q = argv[2]
+    else:
+        q = ""
+    req = requests.post('http://0.0.0.0:5000/search_user', data=q)
+    try:
+        jason_dict = req.json()
+        if json_dict:
+            print(f"[{json_dict['id']}] {json_dict['name']}")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
