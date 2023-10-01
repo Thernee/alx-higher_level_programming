ca71145fb3282b9c3b234f0c5e57d693af227ef5
Abubakar Adam Sani <Athernee@gmail.com>
2023-10-01 14:35:38 +0000

    Add: 7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""sends request to a URL and displays body of the response, or error"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    req = requests.get(argv[1])
+    if req.status_code < 400:
+        print(req.text)
+    else:
+        print(f"Error code: {req.ststus_code}")
